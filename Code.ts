0040e8bb1766788089cfa3196f006ee86e1e542a
sofiso <sofia@smcf.io>
2019-04-03 00:08:16 -0400

    Wrong ts lib oops

--- Code.ts
+++ Code.ts
@@ -1,13 +1,6 @@
-//Remove header row and move "attn" line after city/state!
-//FINISH UPDATESUBJECT or MAKE NEW LIST TO SAVE TIME OPENING FOLDER
-function findFolders(n: string){return DriveApp.getFoldersByName(n);}
-function findFiles(n: string){return DriveApp.getFilesByName(n);}
-function noFolder(n: string){return !findFolders(n).hasNext()}
-function noFile(n: string){return !findFiles(n).hasNext()}
-function open(s: GoogleAppsScript.Drive.File){return SpreadsheetApp.open(s)}
 function printPdf(s: GoogleAppsScript.Drive.File, f: GoogleAppsScript.Drive.Folder){
   let n = s.getName(), tmp = s.makeCopy(n + "tmp_pdf_copy");
-  let url = tmp.getUrl(), id = open(s).getSheetId();
+  let url = tmp.getUrl(), id = SpreadsheetApp.open(s).getSheetId();
   let x = 'export?exportFormat=pdf&format=pdf&fitw=true&portrait=false&gridlines=false&gid=' + id;
   url = url.replace('edit?usp=drivesdk', '');
   let tkn = ScriptApp.getOAuthToken();
@@ -20,116 +13,87 @@ const today = Utilities.formatDate(new Date(), "GMT-5", "MM/dd/yy");
 const master = SpreadsheetApp.getActiveSpreadsheet().getSheets()[1];
 let period = master.getName(), data: Array<Array<any>> = master.getDataRange().getValues().slice(4);
 class Format{
-  n: string; ss: GoogleAppsScript.Drive.File; folder: GoogleAppsScript.Drive.Folder;
-  st: GoogleAppsScript.Drive.File; si: Object[][]; sc: number; fn: string; sub: any; h: Array<any>;
-  constructor(n: string, sc: number, sub: CallableFunction){
-    this.n = n; this.sc = sc; this.sub = sub;
-    this.folder = findFolders(n).next();
-    this.ss = findFiles(n + " DATA").next();
-    this.si = open(this.ss).getDataRange().getValues();
-    this.st = findFiles(this.n + " SUMMARY TEMPLATE").next();
-    this.h = open(this.ss).getRangeByName("headers").getValues()[0];
+  n: string; f: GoogleAppsScript.Drive.Folder; s: Function; h: string[];
+  sf: GoogleAppsScript.Drive.Folder; summary: GoogleAppsScript.Drive.File;
+  ss: GoogleAppsScript.Spreadsheet.Spreadsheet; si: any[][]; sc: number;
+  as: Array<string>; ns: Array<string>; sitem: Function;
+  t: GoogleAppsScript.Drive.File; sn: string;
+  constructor(n: string, sc: number, sn: string, sitem: Function){
+    this.n = n; this.f = DriveApp.getFoldersByName(n).next();
+    this.t = this.f.getFilesByName("TEMPLATE").next(); this.sc = sc;
+    this.ss = SpreadsheetApp.open(this.f.getFilesByName("DATA").next());
+    this.sitem = sitem; this.sf = this.f.getFoldersByName(sn).next();
+    this.si = this.ss.getDataRange().getValues(); this.h = this.si[0];
+    this.as = data.map(x=>x[sc]).filter((x,i,a)=>(i==a.indexOf(x)));; this.sn = sn;
+    this.ns = this.as.filter((x)=>this.si.map(x=>x[0]).indexOf(x)<0);
+    this.summary = this.f.getFilesByName("SUMMARY TEMPLATE").next();
   }
-  activeSubs(){return Array.from(new Set(data.map((x) => x[this.sc])));}
-  knownSubs(){return this.si.map(x=>[x[0], new Map(x.map((y,j)=>[this.h[j],y]))]);}
-  updateSub(sid: string){
-    let a = this.h.map(x=>[]);
-    a[0] = [sid]; ///FIXME - ADD NEW FOLDER AND TEMPLATE
-    open(this.ss).appendRow(a);
-    return new Map(a.map((x,i)=>[this.h[i],x]));
-  } ///WHAT THE HECK IS DOWNLEVEL ITERATION ??
-  matchActiveToKnown(){for (var key of new Map(this.knownSubs.keys())}
-  run(){
-    function folder() {
-      var n = this.fName + " " + today;
-      if (!noFolder(n)) findFolders(n).next().setTrashed(true);
-      return this.folder.createFolder(n);
+  subs(){
+    let l = this.h.length, a = new Array(l-3); a.forEach(x=>[]);
+    function update(s:string){
+      let sf = this.sf, tn = "${s} TEMPLATE";
+      let folders = sf.getFoldersByName(s);
+      let f = (folders.hasNext()) ? folders.next() : sf.createFolder(s);
+      let files = sf.getFilesByName(tn);
+      let t = files.hasNext() ? files.next() : this.t.makeCopy(tn,sf)
+      return [[s],[f.getId()],[t.getId()]];
     }
-    var subs = this.subjects(), billed = [];
-    for (let sub in subs) {
-      printPdf(subs[sub].summary(), folder());
-      billed.push(subs[sub]);
-    };
+    let os:any[][] = this.ss.getDataRange().getValues(), ns = this.ns;
+    let updatens = ns.map(x=>update(x).concat(a));
+    os.shift();
+    let updateks = os.map((x)=>update(x[0]).concat(x.slice(3)));
+    let newsi = updatens.concat(updateks).sort();
+    this.ss.getSheets()[0].getRange(2,1,newsi.length,l).setValues(newsi);
+    SpreadsheetApp.flush();
+    return new Map(newsi.map(x=>[x[0], new Map(this.h.map((y,j)=>[y,x[j]]))]));
+  }
+  items(){
+    let as = this.as, items = new Array(as.length).forEach(x=>[]);
+    data.forEach(x=>items[this.as.indexOf(x[this.sc])].push(this.sitem(x)));
+    return items;
+  }
+  run(){
+    let n = this.n + " " + today, old = this.f.getFoldersByName(n);
+    if (old.hasNext()) old.next().setTrashed(true);
+    let f = this.f.createFolder(n), items = this.items();
+    let subs = this.as.map((x)=>new Subject(subs[x], items[x]));
+    subs.forEach((x:Subject)=>x.run(f));
+    let s = SpreadsheetApp.open(this.summary.makeCopy("SUMMARY",f));
+    subs.forEach((x:Subject)=>x.info.push([this.ns.indexOf(x.id) > -1 ? "new" : "ran"]));
+    let r = s.getSheets()[0].getRange(10,1,this.as.length,subs[0].info.length);
+    r.setValues(subs.map((x:Subject)=>x.info));
+    s.getSheets()[0].getRange(2,2).setValue(subs.length);
   }
 }
-abstract class Subject{
-  id: string; items: Array<Array<any>>; amounts: Array<number>;
-  folder: GoogleAppsScript.Drive.Folder; form: GoogleAppsScript.Drive.File;
-  constructor(a: Array<any>){
-    this.id = a[0], this.folder = DriveApp.getFolderById(a[1]);
-    this.form = DriveApp.getFileById(a[2]), this.items = [], this.amounts = [];
+class Subject{
+  id: string; items: any[][]; info: any[][]; fn: string;
+  f: GoogleAppsScript.Drive.Folder; t: GoogleAppsScript.Drive.File;
+  constructor(a: Map<string,any>, items: any[][]){
+    this.id = a["id"], this.f = DriveApp.getFolderById(a["fix"]);
+    this.t = DriveApp.getFileById(a["t"]); this.items = items;
+    this.info = [[today]].concat(a["n"] ? [a["n"]+1+a["s"], [period]] : [[period]]);
+    this.fn = this.id + "- " + (a["n"]) ? "#" + this.info[1] : period;
   }
-  abstract item(i: Array<any>): Array<any>;
-  total(){return this.amounts.reduce((a,b)=>a+b);}
-}
-class Client extends Subject{
-  nix: boolean; num: Array<number>; fn: string; lastBillDate: Array<any>;
-  constructor(a: any[]){
-    super(a);
-    this.nix = (a[0] == "NIXON"), this.num = [a[3]+1+a[4]],
-    this.fn = a[0] + "- #" + this.num, this.lastBillDate = [a[5]];
-  }
-  item(i: any[]) {return [i[1].concat(i.slice(5,this.nix ? 12:10))]}
-  summary() {
-    let c = open(this.form.makeCopy(this.fn, this.folder)).getSheets()[0];
+  run(folder: GoogleAppsScript.Drive.Folder){
+    let sheet = this.t.makeCopy(this.fn,this.f)
+    let c = SpreadsheetApp.open(sheet).getSheets()[0];
     let l = this.items.length, w = this.items[0].length;
     c.insertRows(16,l);
     SpreadsheetApp.flush();
-    let rs = [c.getRange(16,1,l,w), c.getRange(4,w-1,4), c.getRange(17+l,w)];
-    let vs: Array<Array<any>> = [this.items, [[today], this.num, [period], this.total], [this.total]];
-    rs.forEach((x,i) => x.setValues(vs[i]));
-    c.getRange(16, w, l).setNumberFormat('$0.00')
-    rs[0].setFontSize(10).setWrap(true);
+    c.getRange(16,1,l,w).setValues(this.items).setFontSize(10).setWrap(true);
+    c.getRange(4,2-1,this.info.length).setValues(this.info);
+    c.getRange(16, w, l).setNumberFormat('$0.00');
     SpreadsheetApp.flush();
-    return c;
+    printPdf(sheet,folder);
   }
 }
-class Rider extends Subject{
-  item(i: any[]): any[] {return [1,3,5,6,7,8,9,12,13,14].map(x => i[x])}
-  constructor(a: any[]){super(a)}
-}
 function runInvoices(){
-  let billing = new Format("BILLING", 3, (x: any[])=> new Client(x));
+  function item(i:any[]){return [[i[1]].concat(i.slice(5,i[3] == "NIXON" ? 12:10))]}
+  let billing = new Format("BILLING", 3, "CLIENTS", item);
   billing.run();
 }
 function runPayroll(){
-  let payroll = new Format("PAYROLL", 12, (x: any[]) => new Rider(x));
+  function item(i:any[]){return [i[1]].concat([i[3]], i.slice(5,10), i.slice(12,15))}
+  let payroll = new Format("PAYROLL", 12, "RIDERS", item);
   payroll.run();
 }
-function updateSub(s: any[]){Logger.log(s);}// FIXME
-// function updateSubjects(){
-  //WRITE MIGRATE FUNCTION FOR SUBJECT INFO SHEET, IMPLEMENT THERE
-//   var f = config, d = openSheet(f.sSheet,0),
-//   subs = d.getRange(1,1,d.getLastRow()).getValues();
-//   var sSheet = (noFile(f.nss)) ? SpreadsheetApp.create(f.nss).getSheets()[0]
-//   : openSheet(findFiles(f.nss).next(),0);
-//   sSheet.getRange(1,1,subs.length).setValues(subs);
-//   var data = getData(d), ids = [], runInfo = [];
-//   for (var i=0; i<data.length; i++){
-//     var a = data[i], s = a[0] tn = a[0] + " TEMPLATE";
-//     var fl = (noFolder(s) ? f.folder.createFolder(s) : findFolders(s).next();
-//     var t = (noFile(tn)) ? f.form.makeCopy(tn, fl) : findFiles(tn).next();
-//     ids.push([[fl.getId()], [t.getId()]]);
-//     runInfo.push([[a[6]],[a[7]],[a[5]]]);
-//     f.addressRange(openSheet(t,0)).setValues(f.address(a));
-//   }
-//   sSheet.getRange(1,2,subs.length,2).setValues(ids);
-//   sSheet.getRange(1,3,subs.length,3).setValues(runInfo);
-// }
-
-//function Setup(){
-//const sTemplate = DriveApp.getFilesByName("Billing Summary Template").next(), sName = "Inv. Report - " + today;
-//  sTemplate.makeCopy(sName, f);
-//  this.summary = DriveApp.getFilesByName(sName).next(), this.charges = [];
-//  for (var i=0; i<mData.length; i++) this.charges.push(new Charge(mData[i][2], mData[i]));
-//function makeSummary(run){
-//  var runSummary = SpreadsheetApp.open(run.summary).getSheets()[0], expectedTotal = 0;
-//  for (i=0; i<run.charges.length; i++) expectedTotal+= run.charges[i].amount;
-//  runSummary.getRange("B3:B5").setValues([[today],[billsCreated],[expectedTotal]]);
-//  runSummary.getRange(10, 1, run.newClients.length, run.newClients[0].length).setValues(run.newClients);
-//  runSummary.getRange(30, 1, run.clientIdList.length).setValues(run.clientIdList);
-//  runSummary.getRange("A29").setValue(run.clientIdList.length);
-//}
-//function generateInvoices(run){
-//  for (r=0; r<run.clients.length; r++) run.clients[r].generateInvoice();
-//}
