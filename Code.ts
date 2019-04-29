a09286c568fbbd50103ff5f6af8637c1632c9a94
sofia <sofia@smcf.io>
2019-04-29 19:37:49 -0400

    Add Subject obj, style improvements

--- Code.ts
+++ Code.ts
@@ -1,17 +1,18 @@
 function getFormat(format: string) {//returns format object (billing or payroll)
-  var billing = {name: "BILLING", sc: 3, tc: 'priceColumn', sub: "CLIENTS" };
-  var payroll = {name: "PAYROLL", sc: 12, tc: 'payoutColumn', sub: "COURIERS"};
+  var billing = { name: "BILLING", sc: 3, tc: 'priceColumn', sub: "CLIENTS" };
+  var payroll = { name: "PAYROLL", sc: 12, tc: 'payoutColumn', sub: "COURIERS" };
   //TODO: Add data validation by value (eg "BILLING" & "SWITCH TO PAYROLL")
   return format == "BILLING" ? billing : payroll;
 }
-function getDate(date: Date){return Utilities.formatDate(date, "GMT-5", "M/d/yy");}
+function getDate(date: Date) { return Utilities.formatDate(date, "GMT-5", "M/d/yy"); }
 
-function refresh(f: {sc: number, tc: string, sub: string}){
+function refresh(f: { sc: number, tc: string, sub: string }) {
   var state = dash.getRange("B2").getValue();
   if (state == "NONE") return;
   const input = ss.getSheetByName("INPUT");
   if (state == "INPUT") { //refresh "INPUT" sheet and dash
-    const data = SpreadsheetApp.open(DriveApp.getFilesByName('OSC MASTER INPUT').next()).getSheets()[1].getSheetValues(4, 2, -1, -1);
+    const master = DriveApp.getFilesByName('OSC MASTER INPUT').next();
+    const data = SpreadsheetApp.open(master).getSheets()[1].getSheetValues(4, 2, -1, -1);
     const rows = data.length;
     input.clear().getRange(1, 1, rows, data[0].length).setValues(data).sort(f.sc);
     ss.setNamedRange('priceColumn', input.getRange(1, 13, rows));
@@ -21,50 +22,67 @@ function refresh(f: {sc: number, tc: string, sub: string}){
   ss.setNamedRange('tc', ss.getRangeByName(f.tc));
   ss.setNamedRange('sc', input.getRange(1, f.sc, input.getLastRow()));
   dash.getRange("E2:E").clear().clearDataValidations();
-  var rule = SpreadsheetApp.newDataValidation().requireValueInList(["RUN","PRINT","POST","SKIP"], true).build();
-  dash.getRange(2,5,dash.getSheetValues(2,4,-1,1).length).setDataValidation(rule);
+  var rule = SpreadsheetApp.newDataValidation().requireValueInList(["RUN", "PRINT", "POST", "SKIP"], true).build();
+  var numRows: any = dash.getRange("B8").getValue();
+  dash.getRange(2, 5, numRows).setDataValidation(rule);
 }
 
-function getRunFolder(f: {name: string}, date: string){
+function getKnowns(f: { sub: string }) {//pull data from subject sheet
+  const data: any[][] = ss.getSheetByName(f.sub).getDataRange().getValues();
+  const props = data.shift();
+  const knowns = {};
+  data.forEach(d => {
+    knowns[d[0]] = {};
+    props.forEach((p, i) => knowns[d[0]][p] = d[i]);
+  });
+  return knowns;
+}
+function getItems(f: { sc: number }, actives: any[]) {//pull data from input
+  const items = {};
+  actives.forEach(a => items[a[0]] = []);
+  const input: any[][] = ss.getSheetByName("INPUT").getDataRange().getValues();
+  for (var i = 0; i < input.length; i++) items[input[i][f.sc - 1]].items.push(input[i]);
+  return items;
+}
+function Subject(sub: any[], items: {}, knowns: {}, date: string) {
+  this.state = sub[1];
+  this.total = sub[2];
+  this.items = items[sub[0]];
+  this.data = knowns[sub[0]];
+  this.file = sub[0] + " - # " + (knowns[sub[0]]['statementNum'] || 1) + " - " + date;
+  this.folder = DriveApp.getFolderById(knowns[sub[0]['folder']]);
+  this.print = function(runFolder: GoogleAppsScript.Drive.Folder) {
+    var file = runFolder.getFilesByName(this.file).next();
+    var tmp = file.makeCopy(this.file + "tmp_pdf_copy");
+    let url = tmp.getUrl(), id = SpreadsheetApp.open(file).getSheetId();
+    let x = 'export?exportFormat=pdf&format=pdf&fitw=true&portrait=false&gridlines=false&gid=' + id;
+    url = url.replace('edit?usp=drivesdk', '') + x;
+    let tkn = ScriptApp.getOAuthToken();
+    let r = UrlFetchApp.fetch(url, { headers: { 'Authorization': 'Bearer ' + tkn } });
+    r.getBlob().setName(this.file);
+    tmp.setTrashed(true);
+    DriveApp.getFolderById(this.folder).createFile(r);
+  }
+}
+function getSubjects(f: { sc: number, sub: string }, actives: any[][], date: any) {
+  const items = getItems(f, actives), knowns = getKnowns(f);
+  return actives.map(a => Subject(a, items, knowns, date));
+}
+
+function getRunFolder(f: { name: string }, date: string) {
   var dir = DriveApp.getFoldersByName(f.name).next();
   var rfName = f.name + " " + date;
   if (!dir.getFoldersByName(rfName).hasNext()) dir.createFolder(rfName);
   return dir.getFoldersByName(rfName).next();
 }
-function runReports(f: {sc: number, name: string, sub: string}, actives: any[][], date: string) {
-  var subs = {};
-  actives.forEach(sub=>subs[sub[0]] = {state: sub[1], total: sub[2], items: []});
-  const subData: any[][] = ss.getSheetByName(f.sub).getSheetValues(1,1,-1,-1);
-  const props = subData.shift();
-  subData.forEach(d=>{if (subs[d[0]]) props.forEach((p,i)=>subs[d[0]][p] = d[i])});
-  const input: any[][] = ss.getSheetByName("INPUT").getSheetValues(1,1,-1,-1);
-  for (var i=0; i<input.length; i++) subs[input[i][f.sc-1]].items.push(input[i]);
-  var runFolder = getRunFolder(f, date);
+function runReports(f: { name: string }, subs: {}, date: string) {
   for (var s in subs) {
     var sub = subs[s];
-    sub['file'] = s + " - # " + (sub['statementNum'] || 1) + " - " + date;
-    if (!runFolder.getFilesByName(subs[s]['file']).hasNext()) {
-      Logger.log(s + "didn't have file");
+    var runFolder = getRunFolder(f, date);
+    if (!runFolder.getFilesByName(sub['file']).hasNext()) {
+      DriveApp.getFileById(subs[s]['template']).makeCopy(runFolder);
+      subs[s].state = "PRINT";
     }
-    else {Logger.log(s + "file found");}
+    else { Logger.log(s + "file found"); }
   }
-}
-function print(sub: {name: string, file: string}, runFolder: any) {
-  var folder = DriveApp.getFolderById(sub['folder']);
-  var file = folder.getFilesByName(sub.file).next();
-  var tmp = file.makeCopy(sub.file+"tmp_pdf_copy");
-  let url = tmp.getUrl(), id = SpreadsheetApp.open(file).getSheetId();
-  let x = 'export?exportFormat=pdf&format=pdf&fitw=true&portrait=false&gridlines=false&gid=' + id;
-  url = url.replace('edit?usp=drivesdk', '');
-  let tkn = ScriptApp.getOAuthToken();
-  let r = UrlFetchApp.fetch(url + x,{headers: {'Authorization': 'Bearer ' + tkn}});
-  r.getBlob().setName(sub.file);
-  tmp.setTrashed(true);
-  runFolder.createFile(r);
-}
-function test(){
-  var format = getFormat("BILLING");
-  var actives = dash.getSheetValues(2,4,-1,3);
-  var date = "4/27/19"
-  runReports(format, actives, date);
 }
--- runCode.ts
+++ runCode.ts
@@ -3,10 +3,11 @@ var ss = SpreadsheetApp.getActiveSpreadsheet();
 var dash = ss.getSheetByName("DASH");
 
 function run() {
-  var config: any[] = dash.getSheetValues(1,2,3,1).map(x=>x[0]);
-  var f = getFormat(config[0]);
+  const config: any[] = dash.getSheetValues(1,2,3,1).map(x=>x[0]);
+  const f = getFormat(config[0]);
   refresh(f);
-  var date = getDate(config[2]);
-  var actives: any[][] = dash.getSheetValues(2,4,-1,3);
-  runReports(f, actives, date);
+  const date = getDate(config[2]);
+  const actives: any[][] = dash.getSheetValues(2,4,-1,3);
+  const subjects = getSubjects(f, actives, date);
+  runReports(f, subjects, date);
 }
