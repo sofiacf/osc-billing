25277f59149720d48441fca8d32508c76115b34d
sofiacf <sofia@smcf.io>
2019-04-28 23:20:30 -0400

    Add back pdf print

--- Code.ts
+++ Code.ts
@@ -1,15 +1,13 @@
 //Setup functions for global variables, run every time
 function getFormat() {
-  var state = dash.getRange("B1").getValue();
-  return ((dash.getRange("B1").getValue() == "BILLING") ? { sc: 3, total: [12], subject: "CLIENTS" }
-        : { sc: 12, total: [14, 13], subject: "COURIERS" });
+  return (dash.getRange("B1").getValue() == "BILLING" ?
+    {name: "BILLING", sc: 3, total: [12], subject: "CLIENTS" }
+    : { name: "PAYROLL", sc: 12, total: [14, 13], subject: "COURIERS" });
 }
-function getItems(){
-  //Collects all rows of OSC Master Input sorted by subject
+function getItems(){ //Pull charges into object indexed by subject
   const input = ss.getSheetByName("INPUT").getSheetValues(1,1,-1,-1);
   const items = {};
-  //Since input is read as array, decrease subject column
-  const col = f.sc - 1;
+  const col = f.sc - 1; //Since input is read as array, decrease subject column
   for (var i = 0; i < input.length; i++){
     items[input[i][col]] = (items[input[i][col]] || []).concat(input[i]);
   }
@@ -26,8 +24,8 @@ function getKnowns(){
   return knowns;
 }
 function runReports(){
-  var actives = dash.getRange(2,4,numSubs).getValues().map(x=>x[0]);
-  actives.forEach(x=>knowns[x]);
+  //for actives
+
 }
 
 function refresh(){
@@ -36,7 +34,6 @@ function refresh(){
   if (state == "DASH") { //reset dash formulas
     dash.getRange("D2").setFormula('=ARRAYFORMULA(UNIQUE(subColumn))');
     dash.getRange("F2").setFormula('=ARRAYFORMULA(IF(B1="BILLING",SUMIF(subColumn,UNIQUE(subColumn),priceColumn),SUMIF(subColumn,UNIQUE(subColumn),payoutColumn)))');
-    ss.getRange("B9").setValue(numSubs);
     ss.getRange("E2:E").clear().clearDataValidations();
     var rule = SpreadsheetApp.newDataValidation().requireValueInList(["RERUN","POST","SKIP"], true).build();
     dash.getRange(2,5,numSubs).setDataValidation(rule);
@@ -59,11 +56,21 @@ function post() {
   //if set to all (or selected?), saves pdfs and updates subs/collections sheet
   var state = dash.getRange("B3").getValue();
   if (state == "NONE") return;
-  if (state == "ALL") {
+  if (state == "SELECTED") {
     var actives = dash.getRange(2,4,numSubs).getValues().map(x=>x[0]);
-    actives.forEach(function(a){
-      var fileName = "test";
-      DriveApp.getFolderById(a["folder"]).getFilesByName(fileName);
-    })
+    actives.forEach(function(sub: string){
+      var name = sub + " - #" + knowns[sub]["statementNum"] + " - " + date;
+      var folder = DriveApp.getFolderById(knowns[sub]["folder"]);
+      var file = folder.getFilesByName(name).next();
+      var tmp = file.makeCopy(name+"tmp_pdf_copy");
+      let url = tmp.getUrl(), id = SpreadsheetApp.open(file).getSheetId();
+      let x = 'export?exportFormat=pdf&format=pdf&fitw=true&portrait=false&gridlines=false&gid=' + id;
+      url = url.replace('edit?usp=drivesdk', '');
+      let tkn = ScriptApp.getOAuthToken();
+      let r = UrlFetchApp.fetch(url + x,{headers: {'Authorization': 'Bearer ' + tkn}});
+      r.getBlob().setName(name);
+      tmp.setTrashed(true);
+      runFolder.createFile(r);
+    });
   }
 }
--- runCode.ts
+++ runCode.ts
@@ -1,12 +1,14 @@
 //Global variables, format set in runCode since depends on config value
-var f: { sc: number; total: number[]; subject: string};
+var f: { name: string; sc: number; total: number[]; subject: string};
 var ss = SpreadsheetApp.getActiveSpreadsheet();
 var dash = ss.getSheetByName("DASH");
-var items, knowns, numSubs;
+var date = dash.getRange("B8").getValue();
+var items, knowns, numSubs, runFolder;
 
 function run() {
   //retrieves format object (subject and total columns)
   f = getFormat();
+  runFolder = DriveApp.getFilesByName(f.name + " " + date);
   items = getItems();
   numSubs = Object.keys(items).length;
   knowns = getKnowns();
