5c9644c61309aef85158f6d8dc3c3b66fcda277c
sofia <sofia@smcf.io>
2019-05-04 16:49:02 -0400

    Clean up runCode.ts

--- runCode.ts
+++ runCode.ts
@@ -1,9 +1,4 @@
-//Global variables, format set in runCode since depends on config value
-// var ss = SpreadsheetApp.getActiveSpreadsheet();
-// var dash = ss.getSheetByName("DASH");
 function run() {
   const wkbk: WorkbookManager = new WorkbookManager();
   wkbk.doRun();
-  // const subjects = getSubjects(f, actives, date);
-  // runReports(f, subjects, date);
 }
