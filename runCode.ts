1ed741aa549b1d6537cbde5a85625ae3efb25b93
sofia <sofia@smcf.io>
2019-05-17 22:46:34 -0400

    Add test mode

--- runCode.ts
+++ runCode.ts
@@ -1,4 +1,19 @@
+var execution_modes = {
+  TESTING: 'testing',
+  PRODUCTION: 'production'
+}
 function run() {
   const wkbk: WorkbookManager = new WorkbookManager();
-  wkbk.doRun();
+  let scriptProperties = PropertiesService.getScriptProperties();
+  let mode = scriptProperties.getProperty('execution_mode');
+  switch (mode) {
+    case execution_modes.TESTING:
+      test();
+      break;
+    case execution_modes.PRODUCTION:
+      wkbk.doRun();
+  }
+}
+function test() {
+  Logger.log('test mode');
 }
